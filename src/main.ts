9ae13be764518893e80dacaeab5cfcc7e5b5bff6
phuoc.trancong <phuoc.trancong@vti.com.vn>
2022-07-26 17:04:52 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'files'));
+  await app.listen(3000);
+}
+bootstrap();
